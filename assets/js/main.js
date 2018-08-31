4248ec8dfffbf0a2646d1b34eed64b4ff483d7e6
Poltergeist <brandonssc@gmail.com>
2018-08-31 12:34:02 -0400

    added json structure, some JS functions and comments

---
+++ assets/js/main.js
@@ -0,0 +1,31 @@
+//Code Review Tool.js
+//
+//Contributors: Nick, Emily, Emre, and Brandon
+//
+//Global Variables
+
+//put empty arrays here to store JSON data
+//declare global vars needed
+//reset some vars
+
+//Functions needed
+
+function start (){
+  //initialize game on page-load and reset
+}
+
+function genCards () {
+  //generate flash cards from array, use $.map method
+}
+
+function getJsonArray(){ // change function title
+  //need a function for grabbing json data and putting into empty array?
+}
+
+function showAnswer () {
+  //shows user Answer on click
+}
+
+//probably more functions needed
+
+//on clicks needed
