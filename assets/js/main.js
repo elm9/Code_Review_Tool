0c1e3c05dc35d67d6c4072e06d6df372a4496e46
Poltergeist <brandonssc@gmail.com>
2018-09-01 19:04:42 -0400

    added comments, need to remove alot

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,14 +33,22 @@
 //   //shows user Answer on click
 // }
 
-//Grab the json file data
+//Grab the json file data into array
 var moduleJson = [];
 
 //use count for next/previous flashcard function
 var count = 0;
 
-//fix: this is ugly! make default question better
-// or just put in startModule function
+
+//getJSON
+//put if statements here to get correct module (html, css, etc) to get correct json module file
+//for now only API
+$.getJSON("./assets/data/module-api.json", function(json) {
+  moduleJson = json.cards;
+  console.log(moduleJson);
+  getModule();
+
+});
 
 
 //use this function to make next/previous question functionality?
@@ -67,27 +75,21 @@ function cardToggle() {
 //on click to toggle q & a divs
   $('div.question, div.answer').on('click', function() {
     //output html to card
+    //need to assign count to something, right now default to 0 because of global var count
     $("div.question").html("Question: " + moduleJson[count].question);
     $("div.answer").html("Answer: " + moduleJson[count].answer);
     console.log(moduleJson[count].answer);
     console.log(moduleJson[count].question);
 
     //increase count on click
-
+     //count++;
 
     //toggle between question and answer div
       $('div.answer, div.question').toggle()
     }
   );
 }
-//put if statements here to get correct module (html, css, etc) to get correct json module file
-//for now only API
-$.getJSON("./assets/data/module-api.json", function(json) {
-  moduleJson = json.cards;
-  console.log(moduleJson);
-  getModule();
 
-});
 
 //TODO: NEXT, need to use var count=0, use count to get index(0, 1, 2) on click next count++ (only use count++ after question/answer from ModuleJson array
 // TODO: has been displayed
@@ -105,7 +107,8 @@ cardToggle();
 //doesnt do anything, make another for cardPrev, need to make html divs with id cardNext, and cardPrev
 $("#cardNext").on("click", "#flashCards", function() {
   //clear #flashcards container
-  // $(this).html("<p>On click show back</p>");
+
+  //use function getModule? but rename to getQuestion or something?
 
 }); //end onclick
 
