a2ed17d545efcec15640b68a10e082cd957eba2c
Poltergeist <brandonssc@gmail.com>
2018-09-06 15:28:44 -0400

    updating JS for push to master

--- assets/js/disarray.js
+++ assets/js/disarray.js
@@ -1,40 +1,13 @@
-//Code Review Tool.js
-//
-//Contributors: Nick, Emily, Emre, and Brandon
-//
-//
-//import JSON file object! -google !
-//
-//Global Variables
-//empty array to generate flash cards?
-// var cardsArr = [];
-// var questionsArr = [];
-// var answersArr = [];
-
-//put empty arrays here to store JSON data
-//declare global vars needed
-//reset some vars
-
-//Functions needed
-
-// function startModule () {
-//   //initialize module on page-load and reset
-// }
-//
-// function getJsonArray() { // change function title
-//   //need a function for grabbing json data and putting into empty array? yes
-// }
-//
-// function genCards () {
-//   //generate flash cards from array, use $.map method
-// }
-//
-// function showAnswer () {
-//   //shows user Answer on click
-// }
+// ----------------------------------------------------------------------------------------------------
+// (i) store the q+a arrays 
+// ----------------------------------------------------------------------------------------------------
+// ---------(Landing page)
 
 //Grab the json file data into array
 var moduleJson = [];
+
+
+// ---------(notLanding page)
 
 //array for review again card data
 var reviewArr = [];
@@ -45,165 +18,230 @@ var understandArr = [];
 //use count for next/previous flashcard function
 var count = 0;
 
-var moduleApi = "./assets/data/module-api.json";
 
-//getJSON
-//put if statements here to get correct module (html, css, etc) to get correct json module file
-//for now only API
-$.getJSON(moduleApi, function(json) { //change direct link to json file w variable. to change what it gets on category button click
-  moduleJson = json.cards;
-  console.log(moduleJson);
+var tinyReviewCards = [];
 
-  //display initial question
-  $('div.question').html("<b><u>Question</u></b><br><br>" + moduleJson[count].question);
+var tinyUnderstandCards = [];
 
-  //testing function for now
-  getModule();
 
-});
-
-//TODO: ignore getModule function for now, for testing
-//TODO: change function name! to reflect what it actually does
-//use this function to $.map out and generate flashCard stack divs, for 'i understand / review again' card stacks
-function getModule() {
-  //use $.map instead of for loop ... will get there
-  //how to use $.map to display first index, then add count oo
-  // use if statement inside for loop? with var count = 0 (global), at end put count++, so if at index, go to next index
-
-  for (var i = 0; i < moduleJson.length; i++) {
-    console.log(i, "-", moduleJson[i].id);
-    console.log(i, "-", moduleJson[i].question);
-    console.log(i, "-", moduleJson[i].answer);
-  }
-}
+// ----------------------------------------------------------------------------------------------------
+// (ii) initializing functions
+// ----------------------------------------------------------------------------------------------------
 
 //function to toggle between question/answer on click
 function cardToggle() {
   //question div is hidden bc right now placeholder start text is in html .answer div, on click goes  shows .question div first
+  $('div.answer').hide();
+  //on click to toggle q & a divs
+  $('#flashCards').on('click',
+    function() {
 
-  $('.answer').hide();
-
-
-//on click to toggle q & a divs
-  $('#cardContain').on('click', ".question, .answer", function() {
-
-    //output html to card
-    $(".question").html("<b><u>Question</u></b><br><br>" + moduleJson[count].question);
-    $(".answer").html("<b><u>Answer</u></b><br><br>" + moduleJson[count].answer);
-    console.log(moduleJson[count].answer);
-    console.log(moduleJson[count].question);
+      $('.answer, .question').slideToggle(800);
 
 
-    //toggle between question and answer div
-    //$.toggle does the business
-      $('div.answer, div.question').toggle();
-  }
+      $("div.question").html("<b>Question</b><br><br><br>" + moduleJson[count].question);
+      $("div.answer").html("<b>Answer</b><br><br><br>" + moduleJson[count].answer);
+    }
   );
+
+
 }
 
+// ----------------------------------------------------------------------------------------------------
+// (iii) other functions
+// ----------------------------------------------------------------------------------------------------
 
-//probably more functions needed
-//on clicks needed
+//***this function switches the overall view between the two main containers
+function changeState() {
+    var x = document.getElementById('Landing');
+    var y = document.getElementById('notLanding');
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+        y.style.display = 'none';
+    } else {
+        x.style.display = 'none';
+        y.style.display = 'block';
+    }
+}
 
-
-
-
-
+// generate cards for review
+// function genReviewCards() {
+//   $(".tinyReview").html(reviewArr.id.map(function (genTinyReview) {
+//     return ("<div class='tinyGen' data-topic='" + genTinyReview +"'>" + genTinyReview + '</div>');
+//   }).join(" "));
+// }
 
 //on click toggle between question and answer state
 
 cardToggle();
 
-//genCards - review again array
-//how to display review again /
-function genReviewCards() {
-  $("#reviewCards").html(reviewArr.map(function (btnItems) {
-    return ("<div class='gifGen' data-topic='" + btnItems +"'>" + btnItems + '</div>');
-  }).join(" "));
-}
-//don't call this function
-//genReviewCards();
+
+// ----------------------------------------------------------------------------------------------------
+// (iv) button clicks filled with the correct functions
+// ----------------------------------------------------------------------------------------------------
+//          (A) Landing page
+// when user clicks on a category button:
+// fill Q+A array with Review Again[category that was clicked] array
+// display Q[0] in the Q+A box on notLanding page
+// change state to notLanding page
+// when user clicks on a category I Understand This button:
+// fill Q+A array with I Understand This[category that was clicked] array
+// display Q[0] in the Q+A box on notLanding page
+// change state to notLanding page
+//          (B) notLanding page
+// when user clicks on the Q+A box:
+// switch between Q[i] and A[i]
+// when user clicks on Review Again:
+// adjust Q+A display by i++ to display the next Q
+// if there are no more Questions left in the array: display an alert and go back to the Landing Page
+// when user clicks on I Understand This:
+// take Q[i] and A[i] out of Review Again array and append to I Understand This array
+// adjust Q+A display by i++ to display the next Q
+// if there are no more Questions left in the array: display an alert and go back to the Landing Page
 
 
-$("#html-review").on("click", function () {
-  alert("Handler for .click() called.");
+$("#Landing :Button").on("click", function () {
+    // alert(this.id + "was just clicked!")
+
+    var module = this.id;
+    var getthatmodule = ("./assets/data/" + module + ".json");
+
+    $.getJSON(getthatmodule, function (json) {
+        moduleJson = json.cards;
+        //display initial question
+        $('div.question').html("<b>Question</b><br><br><br>" + moduleJson[count].question);
+        //testing function for now
+    });
+    changeState();
 });
 
-//review and understand button/div functionality -- need to put in functions and clean up? like cardToggle function.
-  $("#reviewContain").on("click", "#review, #understand", function (event) {
+
+
+
+//review and understand button/div functionality -- need to put in functions and clean up?
+$("#reviewContain").on("click", "#review, #understand", function (event) {
 
     event.stopPropagation();
     // console.log(moduleJson[count]);
     // console.log("json array length " + moduleJson.length);
     $('div.answer').hide();
     $('div.question').show();
-
     var id = $(this).attr('id');
-    console.log("target id = " + id);
 
+    //if review again is clicked
     if (id === "review") {
-      reviewArr.push(moduleJson[count]);
-      console.log(reviewArr);
+
+        reviewArr.push(moduleJson[count]);
+
+        //output to tinyCards
+        tinyReviewCards = reviewArr.map(a => a.id);
+
+
+        moduleJson.splice(count, 1); //TODO: FIX THIS!
+        count = count - 1; //OMFG
+
+
+        if (moduleJson.length == 0) {
+            count = 0;
+            var repeat = confirm("Do you want to review the questions you had difficulty with again?")
+            if (repeat) {
+                alert("okay we'll repeat");
+                moduleJson = reviewArr;
+                reviewArr = [];
+
+                //display initial question
+                $('div.question').html("<b>Question</b><br><br><br>" + moduleJson[count].question);
+
+                //array for  i understand card data
+                understandArr = [];
+
+
+            }
+            else {
+                alert("good job, going back to categories");
+                reviewArr = [];
+
+                //array for  i understand card data
+                understandArr = [];
+                changeState();
+            }
+        }
+
+
     }
+    //if i understand clicked
     else if (id === "understand") {
-      understandArr.push(moduleJson[count]);
-      console.log(understandArr);
 
-       //takes current array index out of the main moduleJson array
-      moduleJson.splice(count,1);
-      console.log(moduleJson);
+        understandArr.push(moduleJson[count]);
 
-      if (moduleJson.length == 0) {
-        $("#cardContain").html("Display categories here!");
+        //output to tinyCards
+        tinyUnderstandCards = understandArr.map(a => a.id);
 
-        alert("You've completed this category!");
-      }
+
+        //takes current array index out of the main moduleJson array
+        moduleJson.splice(count, 1); //TODO: FIX THIS!
+        count = count - 1; //OMFG
+
+
+        if ((moduleJson.length === 0) && (reviewArr.length === 0)) {
+
+            alert("congratulations, you understand all of it!")
+            changeState();
+        }
+
+        else if ((moduleJson.length === 0) && (reviewArr.length !== 0)) {
+            count = 0;
+            var repeat = confirm("Do you want to review the questions you had difficulty with again?");
+            if (repeat) {
+                alert("okay we'll repeat");
+                moduleJson = reviewArr;
+                reviewArr = [];
+
+                //display initial question
+                $('div.question').html("<b>Question</b><br><br><br>" + moduleJson[count].question);
+
+                //array for  i understand card data
+                understandArr = [];
+
+
+            }
+            else {
+                alert("good job, going back to categories");
+                reviewArr = [];
+
+                //array for  i understand card data
+                understandArr = [];
+                changeState();
+            }
+
+        }
+
+
     }
-    // console.log(count);
-    count++;
 
+
+
+    // console.log(count);
+    //better place to put this
+    count++;
 
     if (moduleJson.length > count) {
 
+        $("div.question, div.answer").empty();
+        $("div.question").html("<b>Question</b><br><br><br>" + moduleJson[count].question);
+        $("div.answer").html("<b>Answer</b><br><br><br>" + moduleJson[count].answer);
 
+        $("#tinyUnderstand").html(tinyUnderstandCards.map(function (genTinyUnderstand) {
+            return ("<button class='tinyUnderstandGen' data-topic='" + genTinyUnderstand + "'>" + genTinyUnderstand + '</button>');
+        }).join(" "));
 
-      $("div.question, div.answer").empty();
-      $("div.question").html("<b><u>Question</u></b><br><br>" + moduleJson[count].question);
-      $("div.answer").html("<b><u>Answer</u></b><br><br>" + moduleJson[count].answer);
+        $("#tinyReview").html(tinyReviewCards.map(function (genTinyReview) {
+            return ("<button class='tinyReviewGen' data-topic='" + genTinyReview + "'>" + genTinyReview + '</button>');
+        }).join(" "));
 
-      console.log("count after: " + count);
     } else if (moduleJson.length <= count) {
-      count = 0;
-      $("div.question").html("<b><u>Question</u></b><br><br>" + moduleJson[count].question);
-      $("div.answer").html("<b><u>Answer</u></b><br><br>" + moduleJson[count].answer);
+        count = 0;
+        $("div.question").html("<b>Question</b><br><br><br>" + moduleJson[count].question);
+        $("div.answer").html("<b>Answer</b><br><br><br>" + moduleJson[count].answer);
     }
-
-
-
-//need else statement here to go to categories page?
-
-    //TODO: ignore these comments
-
-    //get current array index items moduleJson[i] and push it to reviewArray
-//use count++ to control index
-    //clear #flashcards container
-    //    $("#cardContain").empty();
-       //gen next flashcard array item
-    //use function getModule? but rename to getQuestion or something?
-    //
-
-  }); //end onclick
-
-
-//TODO: onclick dont need!
-
-// doesnt do anything yet, make another for cardPrev, need to make html divs with id cardNext, and cardPrev on each side of flash card output
-// use count++ on next to go through array like.. moduleJson[count].answer/question/etc
-$("#cardNext").on("click", "#flashCards", function() {
-  //clear #flashcards container
-
-  //use function getModule? but rename to getQuestion or something?
-
-}); //end onclick
-
-
+});
